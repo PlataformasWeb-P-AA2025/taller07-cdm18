2f3f5aa6b7478197547477b87a407cbe8e5a60df
Carlos <cdmejia4@utpl.edu.ec>
2025-05-20 09:58:13 -0500

    Commit final uso de .one final

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -1,32 +1,47 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from genera_tablas import Club, Jugador
+from configuracion import cadena_base_datos
 import os
 
-# se importa la clase(s) del archivo genera_tablas
-from genera_tablas import Club, Jugador
-
-from configuracion import cadena_base_datos
-
+# crear motor de conexión a la base de datos
 engine = create_engine(cadena_base_datos)
 
+# crear sesión
 Session = sessionmaker(bind=engine)
 session = Session()
 
-ruta_clubes = os.path.join("data", "datos_clubs.txt")
-ruta_jugadores = os.path.join("data", "datos_jugadores.txt")
+# ruta clubes y jugadores
+data_clubes = os.path.join("data", "datos_clubs.txt")
+data_jugadores = os.path.join("data", "datos_jugadores.txt")
 
-# Leer el archivo y cargar los datos
-with open(ruta_clubes, "r", encoding="utf-8") as archivo:
+# abrir el de clubes y leerlos separando con ;
+with open(data_clubes, "r", encoding="utf-8") as archivo:
     for linea in archivo:
-        partes = linea.strip().split(";")
-        if len(partes) == 3:
-            nombre, deporte, fundacion = partes
-            nuevo_club = Club(nombre=nombre, deporte=deporte, fundacion=int(fundacion))
-            session.add(nuevo_club)
+        datos = linea.strip().split(";")  # Separar por punto y coma
+        if len(datos) == 3:
+            nombre, deporte, fundacion = datos
+            # Crear objeto Club con los datos del archivo
+            club = Club(nombre=nombre, deporte=deporte, fundacion=int(fundacion))
+            session.add(club)  # Agregar a la sesión
 
+# lo mismo pero para jugadores y con formato <CLUB>;<POSICIÓN>;<DORSAL>;<NOMBRE>
+with open(data_jugadores, "r", encoding="utf-8") as archivo:
+    for linea in archivo:
+        datos = linea.strip().split(";")
+        if len(datos) == 4:
+            nombre_club, posicion, dorsal , nombre = datos # formato aplicado
+            # buscar el club correspondiente en la base de datos
+            club = session.query(Club).filter_by(nombre=nombre_club).one()
+            if club:
+                # Crear objeto Jugador asociado al club
+                jugador = Jugador(
+                    club=club,
+                    posicion=posicion,
+                    dorsal=int(dorsal),
+                    nombre=nombre
+                )
+                session.add(jugador)  # Agregar a la sesión
 
-
-session.query(Club).filter_by(nombre="LDU").one()
-
-# se confirma las transacciones
+# commit de la inserción de jugadores
 session.commit()
